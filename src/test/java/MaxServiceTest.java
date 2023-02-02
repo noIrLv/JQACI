aa78d16f5a53cca29933b86d099a52d6480f45cf
Михаил Новиков <spore125@gmail.com>
2023-02-02 23:46:32 +0300

    failTest

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -6,7 +6,7 @@ public class MaxServiceTest {
     public void shouldFindMax(){
         MaxService service = new MaxService();
         int a = 5;
-        int b = 3;
+        int b = 333;
 
         int expected = a;
         int actual = service.max(a, b);
